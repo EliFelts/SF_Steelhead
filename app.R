e84d7a6b1fcbda1a9ac30f89f1b3b56b314365ca
Eli Felts <elifelts@gmail.com>
2025-03-18 11:05:12 -0700

    built initial theme and added slider input that will be the main control for users

--- app.R
+++ app.R
@@ -1,51 +1,52 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
 library(shiny)
+library(tidyverse)
+library(bslib)
+library(DT)
+library(shinyWidgets)
+library(conflicted)
+library(plotly)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+conflicts_prefer(DT::renderDT,
+                 dplyr::filter,
+                 dplyr::lag)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+user_dates <-     
+  sliderInput(inputId = "user_dates",
+              label="Choose a Date Range",
+              min=today()-years(1),
+              max=today(),
+              value=c(today()-months(6),today()))
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+
+ui <- page_navbar(
+  
+  title="South Fork Clearwater Steelhead",
+  
+  theme = bs_theme(preset="cyborg"),
+  
+  sidebar=sidebar(width=500,
+    
+    accordion(
+      accordion_panel(
+        
+        "Options",
+        user_dates
+        
+      )
+      
     )
+    
+  )
+    
+
+  
+  
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
+shinyApp(ui, function(input, output) {})
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+
