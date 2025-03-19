c378a9bd57254df256dae90078cc2bcc3650109e
Eli Felts <elifelts@gmail.com>
2025-03-19 11:35:44 -0700

    Updated data, republishing the app.

--- R/inseason_processing.R
+++ R/inseason_processing.R
@@ -316,26 +316,26 @@ write_feather(projected_pts,"data/projections")
 
 ## get tooltip for points also
 
-cumplot.all <- alldaily %>% 
-  ggplot(aes(x=dummy_sfentry_date,y=daily_cumulative_n,
-             group=spawn_year,color=as.factor(spawn_year)))+
-  geom_line(aes(text=str_c(" Date:",format(dummy_sfentry_date, "%b %d"),
-                           "<br>",
-                           "Spawn Year:",spawn_year,
-                           "<br>",
-                           "Number In:",round(daily_cumulative_n),sep=" ")))+
-  geom_point(data=projected_pts,
-             aes(x=dummy_sfentry_date,y=sy_total,
-                 text=str_c(" Date:",format(dummy_sfentry_date, "%b %d"),
-                            "<br>",
-                            "Projected",projection_category,round(sy_total),
-                            sep=" ")))+
-  theme_bw()+
-  scale_x_date(date_breaks = "1 month", date_labels="%b")+
-  labs(x="Date of entry to South Fork Clearwater",
-       y="Percent of Run Completed",
-       color="")
-cumplot.all
-
-ggplotly(cumplot.all, tooltip=c("text","label")) %>% 
-  layout(hovermode="x")
+# cumplot.all <- alldaily %>% 
+#   ggplot(aes(x=dummy_sfentry_date,y=daily_cumulative_n,
+#              group=spawn_year,color=as.factor(spawn_year)))+
+#   geom_line(aes(text=str_c(" Date:",format(dummy_sfentry_date, "%b %d"),
+#                            "<br>",
+#                            "Spawn Year:",spawn_year,
+#                            "<br>",
+#                            "Number In:",round(daily_cumulative_n),sep=" ")))+
+#   geom_point(data=projected_pts,
+#              aes(x=dummy_sfentry_date,y=sy_total,
+#                  text=str_c(" Date:",format(dummy_sfentry_date, "%b %d"),
+#                             "<br>",
+#                             "Projected",projection_category,round(sy_total),
+#                             sep=" ")))+
+#   theme_bw()+
+#   scale_x_date(date_breaks = "1 month", date_labels="%b")+
+#   labs(x="Date of entry to South Fork Clearwater",
+#        y="Percent of Run Completed",
+#        color="")
+# cumplot.all
+# 
+# ggplotly(cumplot.all, tooltip=c("text","label")) %>% 
+#   layout(hovermode="x")
